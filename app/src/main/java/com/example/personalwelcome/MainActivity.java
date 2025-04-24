b6797ab5d96557b341d2c50ab97eb17aec2ce976
Illia Zaichenko <10376901u@qed.local>
2025-04-24 15:15:21 +0200

    Init commit

---
+++ app/src/main/java/com/example/personalwelcome/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.personalwelcome;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button akcja = findViewById(R.id.akcja);
+        EditText editText = findViewById(R.id.editTextName);
+
+        akcja.setOnClickListener((v)->{
+            String name = editText.getText().toString().trim();
+            if(name == null){
+                showAlertDialog("Błąd","Proszę wpisać swoje imię!", "OK");
+                return;
+            }
+            showAlertDialog();
+        });
+    }
+    private void showAlertDialog(String title,String text, String toastText){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle(title);
+        builder.setMessage(text);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,toastText,Toast.LENGTH_LONG).show();
+
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+        });
+}
